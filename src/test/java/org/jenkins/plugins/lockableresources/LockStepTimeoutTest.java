55f114b7170e6a658a9143e3a0c7609d57b2680b
Tobias Getrost <tgetrost@gnresound.com>
2019-07-31 14:48:34 +0200

    Trying to make the test fail - no success yet

--- src/test/java/org/jenkins/plugins/lockableresources/LockStepTimeoutTest.java
+++ src/test/java/org/jenkins/plugins/lockableresources/LockStepTimeoutTest.java
@@ -11,6 +11,9 @@ import org.junit.Test;
 import org.jvnet.hudson.test.Issue;
 import org.jvnet.hudson.test.JenkinsRule;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 /**
  * Check that locks are released after a timeout
  * @author getrostt
@@ -22,20 +25,32 @@ public class LockStepTimeoutTest extends LockStepTestBase {
   @Issue("JENKINS-50260")
   @Test
   public void timeoutBuildClearsLock() throws Exception {
-    LockableResourcesManager.get().createResource("resource1");
+    LockableResourcesManager.get().createResourceWithLabel("resource1", "my-label");
 
     WorkflowJob p1 = j.jenkins.createProject(WorkflowJob.class, "p");
     String script = "pipeline {\n" +
       "   agent any\n" +
       "   stages {\n" +
       "     stage('test'){\n" +
+      "       failFast true\n" +
       "       options {\n" +
-      "         lock('resource1')\n" +
-      "         timeout(time: 1, unit: 'SECONDS')\n" +
+      "         timeout(time: 5, unit: 'SECONDS', activity: true)\n" +
       "       }\n" +
-      "       steps {\n" +
-      "         echo 'locked!'\n" +
-      "         semaphore 'wait-inside'\n" +
+      "       parallel {\n" +
+      "         stage('p1') {\n" +
+      "           options {\n" +
+      "             lock('my-label')\n" +
+      "           }\n" +
+      "           steps {\n" +
+      "             echo 'locked!'\n" +
+      "             sleep 10\n" +
+      "           }\n" +
+      "         }\n" +
+      "         stage('p2') {\n" +
+      "           steps {\n" +
+      "             sleep 10\n" +
+      "           }\n" +
+      "         }\n" +
       "       }\n" +
       "     }\n" +
       "   }\n" +
@@ -43,11 +58,46 @@ public class LockStepTimeoutTest extends LockStepTestBase {
     p1.setDefinition(
       new CpsFlowDefinition(script));
     WorkflowRun b1 = p1.scheduleBuild2(0).waitForStart();
+
+    //SemaphoreStep.waitForStart("wait-b/1", b1);
+    //SemaphoreStep.success("wait-b/1", null);
+
     j.waitForMessage("locked!", b1);
-    SemaphoreStep.waitForStart("wait-inside/1", b1);
+    //SemaphoreStep.waitForStart("wait-a/1", b1);
     j.waitForCompletion(b1);
     j.assertBuildStatus(Result.ABORTED, b1);
 
-    Assert.assertEquals(1, LockableResourcesManager.get().getFreeResourceAmount("resource1"));
+    Optional<LockableResource> resource1 = LockableResourcesManager.get().getResources().stream()
+      .filter(r -> r.getName().equals("resource1"))
+      .findFirst();
+    Assert.assertTrue(resource1.isPresent());
+    Assert.assertFalse(resource1.get().isLocked());
+
+    Assert.assertEquals(1, LockableResourcesManager.get().getFreeResourceAmount("my-label"));
+  }
+
+  @Issue("JENKINS-50260")
+  @Test
+  public void timeoutBuildClearsLockScripted() throws Exception {
+    LockableResourcesManager.get().createResourceWithLabel("resource1", "my-label");
+
+    WorkflowJob p1 = j.jenkins.createProject(WorkflowJob.class, "p");
+    String script = "timeout(time: 5, unit: 'SECONDS') { lock(label: 'my-label') { semaphore 'wait-a' } }";
+    p1.setDefinition(
+      new CpsFlowDefinition(script));
+    WorkflowRun b1 = p1.scheduleBuild2(0).waitForStart();
+
+    j.waitForMessage("Lock acquired on [Label: my-label]", b1);
+    SemaphoreStep.waitForStart("wait-a/1", b1);
+    j.waitForCompletion(b1);
+    j.assertBuildStatus(Result.ABORTED, b1);
+
+    Optional<LockableResource> resource1 = LockableResourcesManager.get().getResources().stream()
+      .filter(r -> r.getName().equals("resource1"))
+      .findFirst();
+    Assert.assertTrue(resource1.isPresent());
+    Assert.assertFalse(resource1.get().isLocked());
+
+    Assert.assertEquals(1, LockableResourcesManager.get().getFreeResourceAmount("my-label"));
   }
 }
